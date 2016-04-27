ea94a53a31427ada1f8b6219b4cb0fc6398e5f52
Praneeth Bajjuri <praneeth@ti.com>
2016-04-27 00:14:05 -0500

    AM57xevm: Initial Device project baseline
    Initial commit based on jacinto6evm board
    
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,76 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# These two variables are set first, so they can be overridden
+# by BoardConfigVendor.mk
+BOARD_USES_GENERIC_AUDIO := true
+#USE_CAMERA_STUB := true
+OMAP_ENHANCEMENT := true
+
+ifeq ($(OMAP_ENHANCEMENT),true)
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT
+endif
+
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a15
+
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_BLUETOOTH_TI := false
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/ti/am57xevm/bluetooth
+TARGET_NO_BOOTLOADER := true
+
+BOARD_KERNEL_BASE := 0xx80008000
+#BOARD_KERNEL_CMDLINE := console=ttyO2,115200n8 mem=1024M androidboot.console=ttyO2 androidboot.hardware=am57xevmboard vram=20M omapfb.vram=0:16M
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01f00000
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := am57x
+TARGET_BOOTLOADER_BOARD_NAME := am57xevm
+
+BOARD_EGL_CFG := device/ti/am57xevm/egl.cfg
+
+USE_OPENGL_RENDERER := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 805306368
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+TARGET_RECOVERY_FSTAB = device/ti/am57xevm/fstab.am57xevmboard
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB565"
+TARGET_RELEASETOOLS_EXTENSIONS := device/ti/am57xevm
+
+# Connectivity - Wi-Fi
+USES_TI_MAC80211 := false
+ifeq ($(USES_TI_MAC80211),true)
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB  := lib_driver_cmd_wl12xx
+BOARD_HOSTAPD_PRIVATE_LIB         := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE           := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE         := wl12xx_mac80211
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+COMMON_GLOBAL_CFLAGS += -DANDROID_LIB_STUB
+endif
+
+BOARD_SEPOLICY_DIRS := device/ti/am57xevm/sepolicy
---
+++ device.mk
@@ -0,0 +1,129 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+LOCAL_KERNEL := device/ti/am57xevm/kernel
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES := \
+	$(LOCAL_KERNEL):kernel \
+	device/ti/am57xevm/tablet_core_hardware_am57xevm.xml:system/etc/permissions/tablet_core_hardware_am57xevm.xml \
+	device/ti/am57xevm/init.am57xevmboard.rc:root/init.am57xevmboard.rc \
+	device/ti/am57xevm/init.am57xevmboard.usb.rc:root/init.am57xevmboard.usb.rc \
+	device/ti/am57xevm/ueventd.am57xevmboard.rc:root/ueventd.am57xevmboard.rc \
+	device/ti/am57xevm/fstab.am57xevmboard:root/fstab.am57xevmboard \
+	device/ti/am57xevm/media_profiles.xml:system/etc/media_profiles.xml \
+	device/ti/am57xevm/media_codecs.xml:system/etc/media_codecs.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+
+# These are the hardware-specific features
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+
+# Audio
+PRODUCT_COPY_FILES += \
+	device/ti/am57xevm/audio/primary/mixer_paths.xml:system/etc/mixer_paths.xml \
+	device/ti/am57xevm/audio/audio_policy.conf:system/etc/audio_policy.conf
+
+PRODUCT_PROPERTY_OVERRIDES := \
+	hwui.render_dirty_regions=false
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	persist.sys.usb.config=mtp
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := \
+	device/ti/am57xevm/overlay
+
+PRODUCT_PACKAGES += \
+	com.android.future.usb.accessory
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+# WI-Fi
+# PRODUCT_PACKAGES += \
+	wpa_supplicant \
+	wpa_supplicant.conf \
+	wpa_supplicant_overlay.conf \
+	libwpa_client \
+	hostapd \
+	hostapd.conf \
+	dhcpcd.conf \
+	wifical.sh \
+	TQS_D_1.7.ini \
+	TQS_D_1.7_127x.ini \
+	crda \
+	regulatory.bin \
+	wlconf
+
+PRODUCT_PACKAGES += \
+	LegacyCamera \
+	camera_test \
+	ion_tiler_test \
+	iontest \
+	ion_ti_test2 \
+	vpetest \
+	libdrm
+
+# Audio HAL modules
+PRODUCT_PACKAGES += audio.primary.am57x
+PRODUCT_PACKAGES += audio.hdmi.am57x
+# BlueDroid a2dp Audio HAL module
+PRODUCT_PACKAGES += audio.a2dp.default
+# Remote submix
+PRODUCT_PACKAGES += audio.r_submix.default
+
+PRODUCT_PACKAGES += \
+	tinymix \
+	tinyplay \
+	tinycap
+
+# Radio
+# PRODUCT_PACKAGES += \
+	RadioApp \
+	lad_dra7xx \
+	libtiipc \
+	libtiipcutils \
+	libtitransportrpmsg
+
+# Launcher3
+PRODUCT_PACKAGES += Launcher3
+
+# Enable AAC 5.1 decode (decoder)
+PRODUCT_PROPERTY_OVERRIDES += \
+	media.aac_51_output_enabled=true
+
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+$(call inherit-product-if-exists, hardware/ti/dra7xx/jacinto6.mk)
+$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ducati-full_jacinto6evm.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wpan/wl12xx-wpan-fw-products.mk)
