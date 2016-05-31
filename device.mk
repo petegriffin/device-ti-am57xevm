7daa401e82dfe406e54e299fa446814c797538c1
Vishal Mahaveer <vishalm@ti.com>
2016-05-31 14:29:17 -0400

    TEMP: jacinto6evm: disable bluetooth
    Temporarily disable bluetooth for Kernel 4.4 bringup.
    
    Change-Id: Id4d2d4d8200ef3cf8e401a74dfe9833ca1d87548
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- device.mk
+++ device.mk
@@ -127,7 +127,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/ti/dra7xx/jacinto6.mk)
-$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
+#$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ducati-full_jacinto6evm.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
