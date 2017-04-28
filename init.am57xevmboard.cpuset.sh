d6677a7cfdf6d89e51496f39f822a63774cba101
Praneeth Bajjuri <praneeth@ti.com>
2017-04-28 17:34:38 -0500

    am57xevm: update cpuset configuration
    picking commit 86ef7cc803ef ("jacinto6evm: update cpuset configuration")
    
    Change-Id: Icce202ebfa706fae6a65d948c30768f0b7bf4981
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

--- init.am57xevmboard.cpuset.sh
+++ init.am57xevmboard.cpuset.sh
@@ -3,3 +3,4 @@
 # Assign all CPUs for foreground (AM571x and AM572x have different number of cores)
 cat /sys/devices/system/cpu/present > /dev/cpuset/foreground/cpus
 cat /sys/devices/system/cpu/present > /dev/cpuset/foreground/boost/cpus
+cat /sys/devices/system/cpu/present > /dev/cpuset/top-app/cpus
