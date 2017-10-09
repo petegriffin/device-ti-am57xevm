393a9ef5338214cab654a8e4126ad1fcc79a38fa
David Huang <d-huang@ti.com>
2017-10-09 14:12:56 -0500

    am57xevm: Include camera provider modules
    Add the default camera provider to build.
    Camera provider is necessary for camera
    functionality.
    
    Change-Id: Icf78f40bf6a0ba61b30aa62a461761f9934b1b3c
    Signed-off-by: David Huang <d-huang@ti.com>
    [port http://review.omapzoom.org/#/c/38630 to am57xevm project]
    Signed-off-by: Praneeth Bajjuri <praneeth@ti.com>

--- device.mk
+++ device.mk
@@ -36,7 +36,9 @@ PRODUCT_PACKAGES += \
    android.hardware.broadcastradio@1.0-impl \
    android.hardware.soundtrigger@2.0-impl \
    android.hardware.keymaster@3.0-impl \
-   android.hardware.keymaster@3.0-service
+   android.hardware.keymaster@3.0-service \
+   android.hardware.camera.provider@2.4-impl \
+   android.hardware.camera.provider@2.4-service
 
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel \
