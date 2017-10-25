e13b1c1e6aa803198441ef1ac8828f8355dfbdb8
Yongqin Liu <yongqin.liu@linaro.org>
2017-10-25 14:18:31 +0800

    workaround to remove modules that generated as built-in
    and add the dependency for BOARD_RECOVERY_KERNEL_MODULES
    
    Change-Id: I6ba26e9b79f7c230c29d4d685bd6cd0aba3d35d8
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,22 +127,11 @@ BOARD_VENDOR_KERNEL_MODULES := \
 #	$(KERNELDIR)/drivers/scsi/scsi_mod.ko \
 	$(KERNELDIR)/drivers/scsi/sd_mod.ko \
 	$(KERNELDIR)/drivers/usb/storage/usb-storage.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/connector-hdmi.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/dra7-evm-encoder-tpd12s015.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/encoder-sii9022.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/encoder-tc358768.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/encoder-tpd12s015.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/panel-dpi.ko \
 	$(KERNELDIR)/drivers/video/serdes/ti-fpd3-serdes.ko \
-	$(KERNELDIR)/drivers/leds/leds-tlc591xx.ko \
-	$(KERNELDIR)/drivers/video/backlight/gpio_backlight.ko \
-	$(KERNELDIR)/drivers/video/backlight/led_bl.ko \
-	$(KERNELDIR)/drivers/video/backlight/pwm_bl.ko \
 	$(KERNELDIR)/drivers/input/input-polldev.ko \
 	$(KERNELDIR)/drivers/input/touchscreen/edt-ft5x06.ko \
 	$(KERNELDIR)/drivers/input/touchscreen/goodix.ko \
 	$(KERNELDIR)/drivers/input/touchscreen/ldc3001_ts.ko \
-	$(KERNELDIR)/drivers/input/touchscreen/pixcir_i2c_ts.ko
 #	$(KERNELDIR)/drivers/net/wireless/ti/wl18xx/wl18xx.ko \
 #	$(KERNELDIR)/drivers/net/wireless/ti/wlcore/wlcore.ko \
 #	$(KERNELDIR)/drivers/net/wireless/ti/wlcore/wlcore_sdio.ko
@@ -151,12 +140,3 @@ BOARD_RECOVERY_KERNEL_MODULES := \
 #	$(KERNELDIR)/drivers/scsi/scsi_mod.ko \
 	$(KERNELDIR)/drivers/scsi/sd_mod.ko \
 	$(KERNELDIR)/drivers/usb/storage/usb-storage.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/connector-hdmi.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/dra7-evm-encoder-tpd12s015.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/encoder-tc358768.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/encoder-tpd12s015.ko \
-	$(KERNELDIR)/drivers/gpu/drm/omapdrm/displays/panel-dpi.ko \
-	$(KERNELDIR)/drivers/leds/leds-tlc591xx.ko \
-	$(KERNELDIR)/drivers/video/backlight/gpio_backlight.ko \
-	$(KERNELDIR)/drivers/video/backlight/led_bl.ko \
-	$(KERNELDIR)/drivers/video/backlight/pwm_bl.ko \
