dfa0c456b3505997bfff8981bac32920eb1a1ce3
Yongqin Liu <yongqin.liu@linaro.org>
2017-11-09 22:01:45 +0800

    device.mk: inherit dalvik heab settings under frameworks/native/build
    so that we could have the correct dalvik heap settings
    to avoid OutOfMemory errors happen too often
    
    Change-Id: I2e3cc3e94ca525cecad13b32f59351c3fc7c1e1e
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>

--- device.mk
+++ device.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += Launcher3 \
 PRODUCT_PROPERTY_OVERRIDES += \
 	media.aac_51_output_enabled=true
 
-#$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, hardware/ti/dra7xx/am57x.mk)
 #$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
 $(call inherit-product-if-exists, device/ti/proprietary-open/jacinto6/ti-jacinto6-vendor.mk)
